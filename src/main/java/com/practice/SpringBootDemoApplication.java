9cf966f0c2ff15a2384a1b93950aaf2dbb092417
weiwenbo <weiwenbo@wxsbank.com>
2023-08-22 16:04:50 +0800

    Add src folder

---
+++ src/main/java/com/practice/SpringBootDemoApplication.java
@@ -0,0 +1,16 @@
+package com.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @auther: weiwenbo
+ * @Date: 2023/8/22
+ * 概述：
+ */
+@SpringBootApplication
+public class SpringBootDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoApplication.class, args);
+    }
+}
